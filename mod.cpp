5e335a1c43cf1e4d34686a15be824e919da1f9f8
SovjetPWR <37150940+SovjetPWR@users.noreply.github.com>
2024-04-06 01:26:35 +0200

    v1.3.0

---
+++ mod.cpp
@@ -0,0 +1,14 @@
+name = "[A.D.E.] Advanced Diving Environment Realoded by Brigata Lancieri";
+logo = "ade_icon_DiveComputer.paa";
+logoOver = "ade_icon_DiveComputer.paa";
+tooltipOwned = "v1.3.0";
+dlcColor[] = 
+{
+	0.0,
+	0.0,
+	1,
+	1
+};
+overview = "Contains a variety of breathing gases, diving equipment and a dive computer.";
+hideName = 0;
+hidePicture = 0;
