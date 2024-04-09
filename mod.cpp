6afaf059c401823c8f306a41756ed0c9ac713bdb
mrschick <58027418+mrschick@users.noreply.github.com>
2024-04-09 15:14:27 +0200

    Tweak mod.cpp and meta.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,9 +1,9 @@
-name = "[A.D.E.] Advanced Diving Environment Realoded by Brigata Lancieri";
+name = "[A.D.E.] Advanced Diving Environment Reloaded by Brigata Lancieri";
 logo = "ade_icon_DiveComputer.paa";
 logoOver = "ade_icon_DiveComputer.paa";
+overviewPicture = "ade_icon_DiveComputer.paa";
 tooltipOwned = "v1.3.0";
-dlcColor[] = 
-{
+dlcColor[] = {
 	0.0,
 	0.0,
 	1,
