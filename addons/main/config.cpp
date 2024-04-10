9c13e113d0c10f4e55b89754b626c02cc0e29b4f
mrschick <58027418+mrschick@users.noreply.github.com>
2024-04-10 10:37:30 +0200

    Moved CfgSounds to its own file

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -21,17 +21,4 @@ class Extended_PostInit_EventHandlers {
 #include "CfgGlasses.hpp"
 #include "CfgVehicles.hpp"
 #include "CfgWeapons.hpp"
-
-class CfgSounds {
-	sounds[] = {};
-	class ade_sound_zischen {
-		name = "Zischen";
-		sound[] = {QPATHTOF(data\sounds\zischen.ogg), 1, 1};
-		titles[] = {};
-	};
-	class ade_sound_beep {
-		name = "Beep";
-		sound[] = {QPATHTOF(data\sounds\beep.ogg), 1, 1};
-		titles[] = {};
-	};
-};
+#include "CfgSounds.hpp"
